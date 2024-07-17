e5cd2464787a49e2dfe0cf21ea99c65f0baebdc6
Junaid Rasheed <junaidrasheed1@gmail.com>
2024-07-17 18:32:33 +0500

    updated nuxt.config.ts

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   target: 'static',
   router: {
-    base: '/'
+    base: process.env === 'development' ? '/' : '/portfolio'
   },
   ssr: false,
   /*
