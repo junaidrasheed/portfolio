4df88d071b4bbcc0e2398fec27bc34e5cb93fcee
Junaid Rasheed <junaidrasheed1@gmail.com>
2024-07-17 17:26:55 +0500

    gh page branch

---
+++ components/About.vue
@@ -0,0 +1,83 @@
+<template>
+  <!-- TODO: add v-waypoint="{ active: true, callback: onWaypoint }" -->
+  <v-container fluid class="about portfolio-container">
+    <div id="about" class="px-4">
+      <div class="section-title">About me</div>
+      <div class="mb-6 text-center" data-aos="fade-up">
+        <img class="me-img" src="@/assets/images/me.png" />
+      </div>
+      <div class="quotes text-center mb-10" data-aos="fade-up">
+        <q>
+          When you're deeply immersed in a meaningful project that ignites your
+          passion, motivation comes naturally. It's not about being pushed; it's
+          about being drawn by the vision you believe in..
+        </q>
+        <br />
+      </div>
+      <div class="content" data-aos="fade-up">
+        <p>
+          Hi, I'm a motivated full stack software developer with over
+          {{ experience }} years of experience in building web applications
+          using various tools including Vue, React, Ruby on Rails, and Laravel.
+          I'm highly skilled in developing and maintaining both front-end and
+          back-end systems, optimizing performance, and working in agile
+          environments. I'm seeking to leverage my technical skills and
+          collaborative abilities to contribute to innovative projects and help
+          a dynamic team succeed.
+        </p>
+        <p>
+          I also love to photograph and watch a lot of movies. And believe
+          FRIENDS is the greatest sitcom ever!.
+        </p>
+      </div>
+      <div data-aos="fade-up">
+        <Technologies />
+      </div>
+    </div>
+  </v-container>
+</template>
+<script>
+import Technologies from '@/components/Technologies'
+export default {
+  components: {
+    Technologies
+  },
+  computed: {
+    experience: () => {
+      return new Date().getFullYear() - 2017
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.about {
+  color: white;
+
+  .content {
+    padding: 0 3rem;
+    p {
+      font-size: 1.2rem;
+      line-height: 150%;
+    }
+  }
+
+  .quotes {
+    margin: 2rem auto 0;
+    font-size: 2rem;
+    max-width: 50rem;
+  }
+
+  .me-img {
+    height: 12rem;
+    width: 12rem;
+    border-radius: 50%;
+    border: 2px solid rgb(54, 51, 51);
+    object-fit: cover;
+  }
+}
+@media screen and (max-width: 767px) {
+  .content {
+    padding: 0 !important;
+  }
+}
+</style>
---
+++ components/Skills.vue
@@ -0,0 +1,74 @@
+<template>
+  <v-container id="skills" fluid class="skills portfolio-container">
+    <div>
+      <div class="section-title">What I do</div>
+      <v-row no-gutters class="px-5">
+        <v-col class="text-center px-4" cols="12" sm="4">
+          <img
+            alt="frontend"
+            class="skill-img"
+            src="@/assets/images/frontend.png"
+            data-aos="fade-up"
+          />
+          <div class="skill-title" data-aos="fade-up">Frontend</div>
+          <div class="skill-description" data-aos="fade-up">
+            Build responsive and cross browser efficient websites by providing
+            seamless transition from design to development
+          </div>
+        </v-col>
+        <v-col class="text-center px-4" cols="12" sm="4">
+          <img
+            alt="backend"
+            class="skill-img"
+            src="@/assets/images/backend.png"
+            data-aos="fade-up"
+          />
+          <div class="skill-title" data-aos="fade-up">Backend</div>
+          <div class="skill-description" data-aos="fade-up">
+            Maintaining servers including database administration as well as
+            scaling applications to handle load changes.
+          </div>
+        </v-col>
+        <v-col class="text-center px-4" cols="12" sm="4">
+          <img
+            alt="codedesign"
+            class="skill-img"
+            src="@/assets/images/architecture.svg"
+            data-aos="fade-up"
+          />
+          <div class="skill-title" data-aos="fade-up">Architecture</div>
+          <div class="skill-description" data-aos="fade-up">
+            Expert in designing robust and scalable software architecture,
+            ensuring optimal performance and maintainability for complex
+            applications.
+          </div>
+        </v-col>
+      </v-row>
+    </div>
+  </v-container>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.skills {
+  color: white;
+  margin-top: 6rem;
+  .skill-title {
+    font-size: 1.4375rem;
+    font-weight: bold;
+    margin: 1rem 0;
+  }
+  .skill-description {
+    margin-bottom: 3.5rem;
+    font-size: 1rem;
+    line-height: 1.5rem;
+    color: #f5f5f7;
+  }
+  .skill-img {
+    height: 7rem;
+  }
+}
+</style>
